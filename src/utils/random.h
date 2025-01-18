7a8f649cd142cde49bbd574725f1eb38ca273f4f
Nathan Pringle <npringle123@btinternet.com>
2025-01-18 19:31:36 +0000

    random

---
+++ src/utils/random.h
@@ -0,0 +1,6 @@
+#ifndef SAY_IT_WITH_YOUR_CHESST_RANDOM_H
+#define SAY_IT_WITH_YOUR_CHESST_RANDOM_H
+
+unsigned int random_number(unsigned int upper_limit);
+
+#endif //SAY_IT_WITH_YOUR_CHESST_RANDOM_H
